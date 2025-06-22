9640f0c0e06f914c7dec5af5c40756f87cce6410
Alik <moisesalik01@gmail.com>
2025-06-22 15:05:15 -0500

    readme and seed

---
+++ sql/seed.sql
@@ -0,0 +1,12 @@
+-- Active: 1744142670839@@localhost@5432@checkin@public
+INSERT INTO "Rol" (nombre_rol) VALUES
+('Administrador'),
+('Supervisor'),
+('Empleado');
+
+INSERT INTO "Area" (nombre_area) VALUES
+('Recursos Humanos'),
+('Tecnología'),
+('Finanzas'),
+('Logística'),
+('Marketing');
